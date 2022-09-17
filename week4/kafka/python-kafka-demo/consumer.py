9180f71e58aebc128e5bf748e2082f8d6e5f5499
Aramir-A1DATAINT <jeonghyunsuk777@gmail.com>
2022-09-17 16:31:02 +0900

    file delete

--- week4/kafka/python-kafka-demo/consumer.py
+++
@@ -1,9 +0,0 @@
-from kafka import KafkaConsumer
-
-consumer = KafkaConsumer('order_details', bootstrap_servers='localhost:29092')
-
-print("Gonna start listening")
-while True:
-    for message in consumer:
-        print("Here is a message..")
-        print (message)
